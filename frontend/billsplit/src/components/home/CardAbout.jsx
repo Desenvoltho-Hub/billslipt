099febf84c87e5b4abe78ecd3f48e9af790a9551
DesenvolTho <desenvoltho@gmail.com>
2025-11-28 15:39:25 -0300

    Início do frontend

---
+++ frontend/billsplit/src/components/home/CardAbout.jsx
@@ -0,0 +1,9 @@
+function CardAbout() {
+    return (  
+        <div>
+            <h1></h1>
+        </div>
+    );
+}
+
+export default CardAbout;
---
+++ frontend/billsplit/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home(){
+    return (  
+        <div className="grid grid-cols-1 md:grid-cols-3">
+            
+        </div>
+    );
+}
+
+export default Home ;
