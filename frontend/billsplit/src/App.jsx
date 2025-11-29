8bce5e75d3df70e2110cee259618f385bb100692
DesenvolTho <desenvoltho@gmail.com>
2025-11-29 03:35:09 -0300

    Dashboard tomando forma!

--- frontend/billsplit/src/App.jsx
+++ frontend/billsplit/src/App.jsx
@@ -2,17 +2,28 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
 import Dashboard from "./pages/Dashboard";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-      
-          <Route path="/dashboard" element={
+
+        <Route
+          path="/dashboard"
+          element={
             <>
-            <NavBar />
-            <Dashboard />
+              <NavBar />
+              <div className="grid grid-cols-2 md:grid-cols-3">
+                <div>
+                  <SideBar />
+                </div>
+                <div className="col-span-2">
+
+                <Dashboard />
+                </div>
+              </div>
             </>
           }
         />
---
+++ frontend/billsplit/src/components/SideBar.jsx
@@ -0,0 +1,20 @@
+function SideBar() {
+  return (
+    <div className="bg-base-200 h-screen  p-10 ">
+      <div className=" p-5 text-3xl text-center">
+        <span>Dashboard</span>
+      </div>
+      <div className="p-5 text-3xl text-center">
+        <span>Grupos</span>
+      </div>
+      <div className="p-5 text-3xl text-center">
+        <span>Despesas</span>
+      </div>
+      <div className=" p-5 text-3xl text-center"> 
+        <span>Config</span>
+      </div>
+    </div>
+  );
+}
+
+export default SideBar;
